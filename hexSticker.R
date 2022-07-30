4fd4630d04242bc767695ad7fc206e379015666a
Ed Baker <edwbaker@gmail.com>
2022-07-30 20:55:07 +0100

    Create hexSticker.R

---
+++ hexSticker.R
@@ -0,0 +1,13 @@
+library(hexSticker)
+imgurl <- "https://cdn.audioblast.org/bioacoustica_flash.png"
+sticker(imgurl,
+        package="bioacoustica",
+        p_size=20,
+        p_color="white",
+        s_x=1,
+        s_y=.70,
+        s_width=.6,
+        filename="imgfile.png",
+        h_fill = "#353535",
+        h_color = "#888888"
+        )
